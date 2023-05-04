aaca3cdffed05c369c0b6f41ee09e4d2c13a9793
hkup859 <20144454+hkup859@users.noreply.github.com>
2023-05-04 19:52:53 -0400

    Performance tweaks

--- src/components/contactForm.tsx
+++ src/components/contactForm.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { TextField, Checkbox, FormControlLabel, Button, Input } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 import { useState, ChangeEvent } from "react";
 import PhoneNumber from "./phoneNumber";
 
